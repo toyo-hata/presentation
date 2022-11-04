625f06b1c3be7a7906c8c76320a82f31cd3e8c77
toyo-hata <114031743+toyo-hata@users.noreply.github.com>
2022-11-04 19:50:08 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict'
+// 1行目に記載している 'use strict' は削除しないでください
+
+function test(actual, expected) {
+    if (JSON.stringify(actual) === JSON.stringify(expected)) {
+      console.log("Yay! Test PASSED.");
+    } else {
+      console.error("Test FAILED. Keep trying!");
+      console.log("    actual: ", actual);
+      console.log("  expected: ", expected);
+      console.trace();
+    }
+  }
